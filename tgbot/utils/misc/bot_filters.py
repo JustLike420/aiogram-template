89e0ec3a05d880ea6ef26edebfe0a7b87f067e63
Vladimir <44843816+JustLike420@users.noreply.github.com>
2023-03-16 18:31:50 +0300

    fix admin filter

--- tgbot/utils/misc/bot_filters.py
+++ tgbot/utils/misc/bot_filters.py
@@ -7,7 +7,7 @@ from tgbot.data.config import get_admins
 # Проверка на админа
 class IsAdmin(BoundFilter):
     async def check(self, message: types.Message):
-        if message.from_user.id in get_admins():
+        if str(message.from_user.id) in get_admins():
             return True
         else:
             return False
